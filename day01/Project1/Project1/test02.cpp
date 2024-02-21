65f2fdc785fe62895c0b786493a8e9d89b21219b
LEUNSU <lesu002@naver.com>
2024-02-21 14:09:56 +0900

    중간 커밋

--- day01/Project1/Project1/test02.cpp
+++ day01/Project1/Project1/test02.cpp
@@ -1,6 +1,7 @@
 # include <iostream>
 
-int main(void)
+int main(void)	// 괄호가 들어 간 것 = 입력 값(입력값 = 있다 없다), main = 함수명, int = 함수의 출력, 전체= 함수의 정의
+	// 머리 = 3행, 몸체 = 머리 밑 {} 들어간것, void = 출력값 
 {
 	int num = 20;
 	std::cout << "Hello world!" << std::endl;
